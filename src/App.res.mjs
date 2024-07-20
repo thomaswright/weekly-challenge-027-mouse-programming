6d1e318c7f09dce21e51cc2e309db8111c851115
Thomas Wright <thomaswright1@gmail.com>
2024-07-20 16:40:56 -0500

    imp

--- src/App.res.mjs
+++ src/App.res.mjs
@@ -6,18 +6,8 @@ import * as JsxRuntime from "react/jsx-runtime";
 var make = OtherJsx;
 
 function App(props) {
-  return JsxRuntime.jsxs("div", {
-              children: [
-                JsxRuntime.jsx(make, {}),
-                JsxRuntime.jsx("div", {
-                      children: "Dragon Ball Locations!",
-                      className: "w-[500px] text-center text-3xl italic font-black text-[#ff8500]"
-                    }),
-                JsxRuntime.jsx("div", {
-                      children: "Pick a spot on the globe to see where the Dragon Balls go.",
-                      className: "w-[500px] text-center italic font-bold text-[#0049c9]"
-                    })
-              ]
+  return JsxRuntime.jsx("div", {
+              children: JsxRuntime.jsx(make, {})
             });
 }
 
--- src/other.jsx
+++ src/other.jsx
@@ -1,46 +1,147 @@
 import { useEffect, useState } from "react";
 
-function insertTextAtCursor(text) {
-  const textarea = document.getElementById("myTextarea");
+let backspaceSymbol = "⌫";
 
-  const startPos = textarea.selectionStart;
-  const endPos = textarea.selectionEnd;
+let INTERVAL = 1000;
+let PHASESHIFT = 300;
 
-  const currentValue = textarea.value;
-
-  textarea.value =
-    currentValue.substring(0, startPos) + text + currentValue.substring(endPos);
-
-  textarea.selectionStart = textarea.selectionEnd = startPos + text.length;
-
-  textarea.focus();
-}
+let Button = ({ setSelection, text, colorOn, selection }) => {
+  let active = selection === text;
+  return (
+    <div
+      className={[
+        "cursor-default rounded text-4xl  font-black transition-colors duration-50 ease-in-out flex flex-row items-center justify-center border-4 ",
+        colorOn
+          ? active
+            ? "bg-teal-300 text-teal-800 border-teal-600"
+            : "bg-cyan-200 text-cyan-500 border-cyan-400"
+          : "bg-white text-cyan-500 border-cyan-400",
+      ].join(" ")}
+      onMouseEnter={(_) => setSelection((v) => text)}
+      onMouseLeave={(_) => setSelection((v) => (v === text ? "" : v))}
+    >
+      {text}
+    </div>
+  );
+};
 
 let main = () => {
-  let [selection, setSelection] = useState("");
+  let [state, setState] = useState({
+    colorOn: false,
+    text: "",
+    selection: "",
+  });
 
   useEffect(() => {
-    let id = setInterval(() => {
-      insertTextAtCursor(selection);
-    }, 300);
-    return clearInterval(id);
+    let colorOnAndWrite = setInterval(() => {
+      setState((v) => {
+        return {
+          colorOn: true,
+          selection: v.selection,
+          text:
+            v.selection === backspaceSymbol
+              ? v.text.substring(0, v.text.length - 1)
+              : v.text + v.selection,
+        };
+      });
+    }, INTERVAL);
+
+    let colorOff = setTimeout(() => {
+      setInterval(() => {
+        setState((v) => {
+          return {
+            ...v,
+            colorOn: false,
+          };
+        });
+      }, INTERVAL);
+    }, PHASESHIFT);
+
+    return () => {
+      clearInterval(colorOnAndWrite);
+      clearInterval(colorOff);
+    };
   }, []);
 
+  let { selection, text, colorOn } = state;
+
+  let setSelection = (f) => {
+    setState((v) => {
+      return {
+        ...v,
+        selection: f(v.selection),
+      };
+    });
+  };
+
   return (
-    <div>
-      <div className="grid-cols-3">
-        <div onMouseOver={(_) => setSelection(">")}>{">"}</div>
-        <div onMouseOver={(_) => setSelection("<")}>{"<"}</div>
-        <div onMouseOver={(_) => setSelection("+")}>{"+"}</div>
-        <div onMouseOver={(_) => setSelection("-")}>{"-"}</div>
-        <div>{null}</div>
-        <div onMouseOver={(_) => setSelection(".")}>{"."}</div>
-        <div onMouseOver={(_) => setSelection(",")}>{","}</div>
-        <div onMouseOver={(_) => setSelection("[")}>{"["}</div>
-        <div onMouseOver={(_) => setSelection("]")}>{"]"}</div>
+    <div className="p-3 ">
+      <div className="font-black text-5xl w-80 text-center text-pink-500 mb-2">
+        {"BFDDR"}
+      </div>
+      <div className="grid grid-cols-3 gap-1 w-80 h-80 mb-2">
+        <Button
+          selection={selection}
+          text={"."}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={"+"}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={","}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={"<"}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={backspaceSymbol}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={">"}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={"["}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={"-"}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
+        <Button
+          selection={selection}
+          text={"]"}
+          setSelection={setSelection}
+          colorOn={colorOn}
+        />
       </div>
       <div>
-        <textarea id="myTextarea" />
+        <div
+          style={{ overflowWrap: "break-word" }}
+          className="my-1 w-80 bg-teal-100 border-4 border-teal-400 text-teal-900 rounded min-h-40 p-2 font-black text-xl leading-none "
+        >
+          {text}
+        </div>
       </div>
     </div>
   );
